7f90c140832310e5ccad30b1dd91d16a8e117622
KUSHANG Suratwala <suratwalakushang@gmail.com>
2022-03-05 17:29:52 +0530

    Carousel

---
+++ GuniKitchenProject/Models/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace GuniKitchenProject.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+
+        [Display(Name ="Customer Id")]
+        [ForeignKey(nameof(Order.User))]
+        public Guid UserId { get; set; }
+        public MyIdentityUser User { get; set; }
+
+
+        [Display(Name = "Product Name")]
+        [Required]
+        [ForeignKey(nameof(Order.Product))]
+        public int ProductId { get; set; }
+        [Display(Name = "Product Name")]
+        public Product Product { get; set; }
+
+
+        public int Quantity { get; set; }
+
+        public int Prize { get; set; }
+
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime OrderDate { get; set; }
+
+
+
+
+    }
+}
---
+++ GuniKitchenProject/Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuniKitchenProject.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+
+        [Display(Name = "Product Category")]
+        [Required(ErrorMessage = "{0} cannot be empty.")]
+        public string ProductCategory { get; set; }
+
+        [Display(Name = "Product Name")]
+        [Required(ErrorMessage = "{0} cannot be empty.")]
+        [StringLength(20, ErrorMessage = "{0} cannot be more than {1} characters.")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Product Description")]
+        [Required(ErrorMessage = "{0} cannot be empty.")]
+        [StringLength(100, ErrorMessage = "{0} cannot be more than {1} characters.")]
+        public string ProductDescription { get; set; }
+
+        [Display(Name = "Product Image")]
+        [Required(ErrorMessage = "{0} cannot be empty.")]
+        public string ProductImage { get; set; }
+
+        [Display(Name = "Product Price")]
+        [Required(ErrorMessage = "{0} cannot be empty.")]
+        public decimal ProductPrice { get; set; }
+
+        [Display(Name = "Product Size")]
+        public string ProductSize { get; set; }
+    }
+}
